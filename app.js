3302c985a2ad6f01faa2c0d9df6f7adab0eec840
Alvinda_akbar10 <89963240+Nandaakbar15@users.noreply.github.com>
2025-09-20 18:51:49 +0700

    Add vercel.json files

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 const express = require("express");
-const app = express();
-const port = 3000;
 const cors = require("cors");
-const router = require('./routes/route');
+const router = require("./routes/route");
 
-// Tambahkan impor untuk Swagger
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
-// Opsi untuk swagger-jsdoc
+const app = express();
+
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'User Management API',
-            version: '1.0.0',
-            description: 'Dokumentasi untuk User Management API'
-        },
-        servers: [
-            {
-                url: `http://localhost:${port}`
-            }
-        ]
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "User Management API",
+      version: "1.0.0",
+      description: "Dokumentasi untuk User Management API",
     },
-    apis: ['./routes/*.js'] // Pastikan path ini benar
+    servers: [
+      {
+        url: "/",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
 };
 
 const specs = swaggerJsdoc(options);
@@ -32,19 +30,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// Tambahkan middleware Swagger UI sebelum router utama
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+// Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(router);
 
-app.listen(port, () => {
-    console.log(`Server is running at : http://localhost:${port}`);
-    console.log(`Dokumentasi API tersedia di http://localhost:${port}/api-docs`);
+app.get("/", (req, res) => {
+  res.status(200).json({
+    statusCode: 200,
+    message: "User Manajemen App",
+  });
 });
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        statusCode: 200,
-        message: 'User Manajemen App'
-    });
-});
+
+module.exports = app;
