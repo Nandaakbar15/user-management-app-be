ab40e5bde6cb06882114135168898819cda48713
Alvinda_akbar10 <89963240+Nandaakbar15@users.noreply.github.com>
2025-09-20 19:12:15 +0700

    Add README.MD

--- api/docs.js
+++ api/docs.js
@@ -1,20 +1,27 @@
 module.exports = (req, res) => {
+  res.setHeader("Content-Type", "text/html");
   res.send(`
     <!DOCTYPE html>
     <html>
       <head>
         <meta charset="UTF-8">
         <title>Swagger UI</title>
-        <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+        <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
       </head>
       <body>
         <div id="swagger-ui"></div>
-        <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+        <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+        <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js"></script>
         <script>
           window.onload = () => {
-            SwaggerUIBundle({
+            window.ui = SwaggerUIBundle({
               url: '/api/swagger',
               dom_id: '#swagger-ui',
+              presets: [
+                SwaggerUIBundle.presets.apis,
+                SwaggerUIStandalonePreset
+              ],
+              layout: "BaseLayout"
             });
           };
         </script>
